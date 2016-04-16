d9ebaa815e0a45fb2d3118677d8a058a80bf1399
Jan Harkes <jaharkes@cs.cmu.edu>
2016-04-16 13:30:18 -0400

    Updates to the validation script, including CVS commit/tag-signing.

--- treebuilder.py
+++ treebuilder.py
@@ -3,6 +3,7 @@ import os
 import re
 import shutil
 from subprocess import check_call, check_output
+from collections import OrderedDict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--release')
@@ -23,7 +24,7 @@ LWP_TAGS = load_tags('lwp.lift')
 RPC2_TAGS = load_tags('rpc2.lift')
 RVM_TAGS = load_tags('rvm.lift')
 
-RELEASES = {}
+RELEASES = OrderedDict()
     
 def rebuild_release(coda):
     lwp, rpc2, rvm = RELEASES[coda]
@@ -59,10 +60,10 @@ def rebuild_release(coda):
 
     # make it look like a proper git checkout
     check_call(['rsync', '-a', "final-git/.git/", "coda/.git"])
-    check_call(['git', '-C', "coda", "reset", "-q", coda_git])
-    check_call('git -C coda ls-files | grep .cvsignore | git -C coda checkout-index --stdin', shell=True)
-    check_call('git -C coda ls-files | grep .gitignore | git -C coda checkout-index --stdin', shell=True)
-    check_call(['git', '-C', "coda", "add", "."])
+    check_call(['git', '-C', "coda", "reset", "--mixed", "-q", coda_git])
+    check_call('git -C coda ls-files | grep .cvsignore | git -C coda checkout-index -f --stdin', shell=True)
+    check_call('git -C coda ls-files | grep .gitignore | git -C coda checkout-index -f --stdin', shell=True)
+    check_call('git -C coda add .', shell=True)
 
 
 for line in open('releases'):
@@ -75,15 +76,29 @@ if args.release:
     rebuild_release(args.release)
 
 else:
-    for coda_git in sorted(RELEASES):
+    for coda_git in RELEASES:
         print("Building release: {}".format(coda_git))
         rebuild_release(coda_git)
 
-        out = check_output(['git', '-C', 'coda', 'status'])
+        out = check_output('git -C coda status', shell=True)
         out += '\n'
         out += check_output(['git', '-C', 'coda', 'diff', '--patch-with-stat', '--minimal', '-C', '--find-copies-harder', '--irreversible-delete', coda_git])
         with open('reports/{}'.format(coda_git), 'w') as f:
             f.write(out)
+
+        msg = """CVS: {} {}""".format(coda_git, ' '.join(r for r in RELEASES[coda_git] if r != 'none') or '')
+        check_call(['git', '-C', 'coda-final', 'checkout', '-f', 'refs/tags/{}'.format(coda_git)])
+        commit_date = check_output('git -C coda-final show -q --pretty=format:%ct', shell=True).strip()
+        env = ['env',
+            'GIT_DIR=coda-final/.git',
+            'GIT_AUTHOR_DATE="{}"'.format(commit_date),
+            'GIT_COMMITTER_DATE="{}"'.format(commit_date),
+        ]
+        check_call(env + ['git', '-C', 'coda', 'add', '.'])
+        check_call(env + ['git', '-C', 'coda', 'commit', '-a', '-m', msg])
+
+        env = ['env', 'GIT_DIR=coda-final/.git']
+        check_call(env + ['git', '-C', 'coda', 'tag', '-f', '-s', '-m', msg, coda_git])
 
         shutil.rmtree('coda')
 
